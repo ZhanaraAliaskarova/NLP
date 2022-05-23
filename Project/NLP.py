082b549f761dc955b739a7d3245541cf6569053f
lskrvzhnr <41797694+lskrvzhnr@users.noreply.github.com>
2022-05-23 17:00:52 +0600

    Add files via upload

---
+++ Project/NLP.py
@@ -0,0 +1,24 @@
+from cgitb import text
+from email.mime import audio
+import speech_recognition 
+import pyttsx3
+
+recognizer = speech_recognition.Recognizer()
+
+while True:
+
+    try:
+
+        with speech_recognition.Microphone() as mic:
+
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+            print(f"Say smth {text}")
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            print(f"Recognized {text}")
+    except speech_recognition.UnknownValueError():
+
+        recognizer = speech_recognition.Recognizer()
+        continue
